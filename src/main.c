8c1e4e323425d95b2ce49513426a2b925166b739
Mateus Mello <mateus.mello.oliva@gmail.com>
2021-04-12 21:16:05 -0300

    ajustado driverbuttons lib

--- src/main.c
+++ src/main.c
@@ -1,5 +1,15 @@
+#include "system_tm4c1294.h"
+#include "driverleds.h"
+#include "driverbuttons.h"
+#include "cmsis_os2.h"
+#include "stdbool.h"
+#include "inc/hw_memmap.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/gpio.h"
+#include "driverlib/interrupt.h"
 
-int main()
+void main(void)
 {
-  return 0;
-}
+    SystemInit();
+    LEDInit(LED4 | LED3 | LED2 | LED1);
+} // main
